9a071d8a960860aded30a42afd910865f387bc84
mustangkong <mustang2247@gmail.com>
2017-10-18 00:19:09 +0800

    update cache

---
+++ src/main/java/com/mybitop/gameversioncontrol/message/Receiver.java
@@ -0,0 +1,26 @@
+package com.mybitop.gameversioncontrol.message;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 消息接受者
+ */
+public class Receiver {
+    private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        logger.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}
