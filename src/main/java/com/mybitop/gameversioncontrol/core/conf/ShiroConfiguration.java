eddce64a3896c4039d0ea861e8e98fb89480b4f4
mustangkong <mustang2247@163.com>
2017-11-14 14:05:15 +0800

    update api

--- src/main/java/com/mybitop/gameversioncontrol/core/conf/ShiroConfiguration.java
+++ src/main/java/com/mybitop/gameversioncontrol/core/conf/ShiroConfiguration.java
@@ -58,8 +58,8 @@ public class ShiroConfiguration {
         //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
         filterChainDefinitionMap.put("/logout.html", "logout");
 
-        filterChainDefinitionMap.put("/check/*", "anon");
-        filterChainDefinitionMap.put("/conf/*", "anon");
+        filterChainDefinitionMap.put("/check/**", "anon");
+        filterChainDefinitionMap.put("/conf/**", "anon");
 
         //<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
         //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
