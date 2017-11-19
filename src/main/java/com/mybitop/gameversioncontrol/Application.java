488cf9866403cf6a1cb63c51bb46277f0e699fb3
mustangkong <mustang2247@gmail.com>
2017-11-19 13:57:07 +0800

    update jpa

--- src/main/java/com/mybitop/gameversioncontrol/Application.java
+++ src/main/java/com/mybitop/gameversioncontrol/Application.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication
 @EnableCaching
+@EnableAutoConfiguration
 //@ControllerAdvice
 public class Application implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
